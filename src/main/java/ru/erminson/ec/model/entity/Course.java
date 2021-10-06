332b65b655bb3fb493e692e3aea6302fb87c73f1
Lev Erminson <erminson@gmail.com>
2021-10-06 11:16:50 +0300

    Add @AllArgsConstructor notation to Course and Topic entities

--- src/main/java/ru/erminson/ec/model/entity/Course.java
+++ src/main/java/ru/erminson/ec/model/entity/Course.java
@@ -1,10 +1,12 @@
 package ru.erminson.ec.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class Course {
     private String title;
     private List<Topic> topics;
--- src/main/java/ru/erminson/ec/model/entity/Topic.java
+++ src/main/java/ru/erminson/ec/model/entity/Topic.java
@@ -1,8 +1,10 @@
 package ru.erminson.ec.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Topic {
     private String title;
     private Integer durationInHours;
